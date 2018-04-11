f5bdc132483fb00b81ba284173be33761b57f388
witmoyl <vagercai@gmail.com>
2018-04-11 13:17:32 +0800

    Update trap.scala

--- scala/trap.scala
+++ scala/trap.scala
@@ -1,4 +1,7 @@
-//在scala中，如果我们使用val型的变量使用下划线赋值的话，就会出现这种提示，如果是使用var型的变量，就不会有这种提示了。
+/*
+  在scala中，如果我们使用val型的变量使用下划线赋值的话，就会出现这种提示，如果是使用var型的变量，就不会有这种提示了。
+  在一下例子中, “_” 的意思是缺省值，而不是如lamda function一般代表一個argument
+*/
 trait Foo{
   def test: Unit
 }
@@ -10,7 +13,7 @@ val fooInstance = new Foo {
 fooInstance.test
 
 /*
-Error:(6, 30) unbound placeholder parameter
+Error:(6, 30) unbound placeholder parameter <- 即此value需要被initialized
   override def test: Unit = _
                             ^
 */
